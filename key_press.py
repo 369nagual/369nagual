d81d0b9d577b07cb5e1498417b0c646acfc0f7cd
369nagual <369nagual@gmail.com>
2022-06-22 14:36:46 +0300

    some more clean up in key_press.py

--- key_press.py
+++ key_press.py
@@ -7,18 +7,6 @@ def main():
     import urllib3
     import pyperclip
 
-    # проверяем регистрацию пакета
-
-    class MyHTMLParser(HTMLParser):
-        def handle_starttag(self, tag, attrs):
-            print("Encountered a start tag:", tag)
-
-        def handle_endtag(self, tag):
-            print("Encountered an end tag :", tag)
-
-        def handle_data(self, data):
-            print("Encountered some data  :", data)
-
     def vk(info=False):
         if info is True:
             print("vk() ==>", end=" ")
@@ -137,37 +125,7 @@ def main():
     time.sleep(2)
     vk_music(sng=song, info=True)
     next_music()
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    #
     os.system('spd-say "Finished"')
-
     restart = pyautogui.confirm(text="Program is finished!", title="Finished", buttons=['Restart', 'Exit'])
     if restart == "Restart":
         main()

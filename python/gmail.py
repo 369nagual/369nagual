3affc98262294f73f792027b2a1aa1c85fed0838
369nagual <369nagual@gmail.com>
2022-06-27 00:41:29 +0300

    send database info to gmail

--- python/gmail.py
+++ python/gmail.py
@@ -1,26 +1,24 @@
-# # Python code to illustrate Sending mail from
-# # your Gmail account
-# import smtplib
-# import test
-#
-# my_email = "vitkashubin@gmail.com"
-# user_password = "Yustas2008/2020"
-# app_password = "yzaeerlxhbbkldot"
-# # creates SMTP session
-# s = smtplib.SMTP('smtp.gmail.com', 587)
-#
-# # start TLS for security
-# s.starttls()
-#
-# # Authentication
-# s.login(my_email, app_password)
-#
-# # message to be sent
-# message = "TEST PYTHON TEST"
-#
-# # sending the mail
-# s.sendmail(my_email, "vitkashubin@mail.ru", message)
-#
-# # terminating the session
-# s.quit()
-#
+# Python code to illustrate Sending mail from
+# your Gmail account
+import smtplib
+from database import data
+
+my_email = "vitkashubin@gmail.com"
+user_password = "Yustas2008/2020"
+app_password = "yzaeerlxhbbkldot"
+# creates SMTP session
+s = smtplib.SMTP('smtp.gmail.com', 587)
+
+# start TLS for security
+s.starttls()
+
+# Authentication
+s.login(my_email, app_password)
+
+# message to be sent
+message = data
+# sending the mail
+s.sendmail(my_email, "vitkashubin@mail.ru", message)
+
+# terminating the session
+s.quit()

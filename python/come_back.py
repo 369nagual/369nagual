3ec542463a4b2b3e0096a40731931c6eca69de59
369nagual <369nagual@gmail.com>
2022-06-29 14:06:41 +0300

    come_back.py cleared

--- python/come_back.py
+++ python/come_back.py
@@ -2,8 +2,6 @@ import pyautogui
 import time
 import os
 
-flag = True
-
 
 def first_func():
     if (login == "Nagual" or login == "nagual") and password == correct_password:
@@ -78,6 +76,8 @@ def run_sound():
                 return first_func()
 
 
+flag = True
+
 while flag:
     correct_password = "Yustas2008"
     login = pyautogui.prompt(text='Login:', title='Title', default='Nagual')
@@ -89,49 +89,3 @@ while flag:
         flag = False
     else:
         pyautogui.alert("Failed")
-
-# while flag:
-#     login = pyautogui.prompt(text='Login:', title='Title', default='Nagual')
-#     password = pyautogui.password(text='Password:', title='Title', default='Yustas2008', mask='*')
-#
-#     if (login == "Nagual" or login == "nagual") and password == "Yustas2008":
-#         text = "Correct Login and Password"
-#         pyautogui.alert(text)
-#         choice = pyautogui.confirm(text='Выберете функцию', title='Функции', buttons=['Exit', 'Paint'])
-#
-#         distance = 200
-#         if choice == "Exit":
-#             dilay = pyautogui.confirm(text="Через колько секунд начать", title="Начать",
-#                                       buttons=["1", "2", "3", "4", "5", "Отмена"])
-#             if dilay == "Отмена":
-#                 flag = False
-#             else:
-#                 dilay = int(dilay)
-#                 time.sleep(dilay)
-#                 exit_point_locate = pyautogui.locateOnScreen("exit_point.png")
-#                 exit_point = pyautogui.center(exit_point_locate)
-#                 x, y = exit_point
-#                 pyautogui.moveTo(x=x, y=y)
-#                 pyautogui.click(
-#
-#                 )
-#
-#         elif choice == "Paint":
-#             dilay = pyautogui.confirm(text="Через колько секунд начать", title="Начать",
-#                                       buttons=["1", "2", "3", "4", "5", "Отмена"])
-#             if dilay == "Отмена":
-#                 flag = False
-#             else:
-#                 time.sleep(dilay)
-#                 while distance > 0:
-#                     pyautogui.drag(distance, 0, duration=0)  # move right
-#                     distance -= 5
-#                     pyautogui.drag(0, distance, duration=0)  # move down
-#                     pyautogui.drag(-distance, 0, duration=0)  # move left
-#                     distance -= 5
-#                     pyautogui.drag(0, -distance, duration=0)  # move up
-#
-#     elif password == "":
-#         flag = False
-#     else:
-#         pyautogui.alert("Failed")

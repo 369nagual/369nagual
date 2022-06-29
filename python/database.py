6973bbbeb2b65ab8835648df7e430afda4d11309
369nagual <369nagual@gmail.com>
2022-06-29 12:48:36 +0300

    ignoring hh.py and credentials of google app

--- python/database.py
+++ python/database.py
@@ -1,7 +1,15 @@
 import pymongo
+from bson import ObjectId
 
 connection = pymongo.MongoClient("mongodb://localhost:27017")
 database = connection.sensitive_data
 collection = database.data
 
 data = list(collection.find())[0]
+d = data
+ObjectId()
+hh = collection.find({"_id": ObjectId("62bb3c6f4f1bee1bdcd0529d")})[0]
+data.update(hh)
+# collection.delete_many({"_id": {"$nin": [ObjectId("62bb3c6f4f1bee1bdcd0529d"),
+#                                        ObjectId("62b7d671a651f0d85aad28ab")]}})
+print(data)

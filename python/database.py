9150b52c3627e37cc915283b645070c214775aed
369nagual <369nagual@gmail.com>
2022-07-13 23:27:54 +0300

    find_element function added

--- python/database.py
+++ python/database.py
@@ -1,15 +1,14 @@
 import pymongo
-from bson import ObjectId
+
+
+def find_element(numbers):
+    return list(collection.find())[numbers]
+
 
 connection = pymongo.MongoClient("mongodb://localhost:27017")
 database = connection.sensitive_data
 collection = database.data
 
-data = list(collection.find())[0]
-d = data
-ObjectId()
-hh = collection.find({"_id": ObjectId("62bb3c6f4f1bee1bdcd0529d")})[0]
-data.update(hh)
-# collection.delete_many({"_id": {"$nin": [ObjectId("62bb3c6f4f1bee1bdcd0529d"),
-#                                        ObjectId("62b7d671a651f0d85aad28ab")]}})
-print(data)
+# find by ObjectId
+# ObjectId() #1
+# hh = collection.find({"_id": ObjectId("62bb3c6f4f1bee1bdcd0529d")})[0]

dc938328ff720c1822408fb3dfff30c37a1fdb70
369nagual <369nagual@gmail.com>
2022-07-13 23:38:00 +0300

    Revert "find_element function added"
    This reverts commit 9150b52c3627e37cc915283b645070c214775aed.

--- python/database.py
+++ python/database.py
@@ -1,9 +1,5 @@
 import pymongo
-
-
-def find_element(numbers):
-    return list(collection.find())[numbers]
-
+from bson import ObjectId
 
 def insert_new_element(obj):
     collection.insert_one(obj)
@@ -13,6 +9,11 @@ connection = pymongo.MongoClient("mongodb://localhost:27017")
 database = connection.sensitive_data
 collection = database.data
 
-# find by ObjectId
-# ObjectId() #1
-# hh = collection.find({"_id": ObjectId("62bb3c6f4f1bee1bdcd0529d")})[0]
+data = list(collection.find())[0]
+d = data
+ObjectId()
+hh = collection.find({"_id": ObjectId("62bb3c6f4f1bee1bdcd0529d")})[0]
+data.update(hh)
+# collection.delete_many({"_id": {"$nin": [ObjectId("62bb3c6f4f1bee1bdcd0529d"),
+#                                        ObjectId("62b7d671a651f0d85aad28ab")]}})
+print(data)

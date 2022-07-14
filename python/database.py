0e46a55684123f61e73e10e76111868785f80a7e
369nagual <369nagual@gmail.com>
2022-07-14 04:28:48 +0300

    function delete_last_element added

--- python/database.py
+++ python/database.py
@@ -10,6 +10,11 @@ def insert_new_element(obj):
     collection.insert_one(obj)
 
 
+def delete_last_element():
+    element = list(collection.find())[-1]
+    collection.delete_one({'_id': ObjectId(element["_id"])})
+
+
 connection = pymongo.MongoClient("mongodb://localhost:27017")
 database = connection.sensitive_data
 collection = database.data

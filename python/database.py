cfc3eaf4bc184496cd09a382de530873eb867dd1
369nagual <369nagual@gmail.com>
2022-07-14 04:32:10 +0300

    comments deleted

--- python/database.py
+++ python/database.py
@@ -18,7 +18,3 @@ def delete_last_element():
 connection = pymongo.MongoClient("mongodb://localhost:27017")
 database = connection.sensitive_data
 collection = database.data
-
-# find by ObjectId
-# ObjectId() #1
-# hh = collection.find({"_id": ObjectId("62bb3c6f4f1bee1bdcd0529d")})[0]
